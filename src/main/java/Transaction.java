983a861eb911331857541ffe071bfd77098d369d
Zero-slayer <71024460+Zero-slayer@users.noreply.github.com>
2021-02-16 09:45:39 +0100

    Created transaction.java

--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Transactions {
+import java.util.Date;
+
+public class Transaction {
+    int amount;
+    Date date;
+
+    public Transaction(int amount, Date date) {
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
 }
