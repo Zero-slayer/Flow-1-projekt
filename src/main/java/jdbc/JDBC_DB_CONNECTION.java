0b0c943e097ac61652dd28c1e0e845362a89b471
Zero-slayer <71024460+Zero-slayer@users.noreply.github.com>
2021-02-16 14:01:32 +0100

    Created JDBC_DB_CONNECTION.java

---
+++ src/main/java/jdbc/JDBC_DB_CONNECTION.java
@@ -0,0 +1,74 @@
+package jdbc;
+
+import java.sql.*;
+import java.util.Calendar;
+
+public class JDBC_DB_CONNECTION implements AutoCloseable{
+
+    private Connection connection;
+    private String db_url;
+    private String db_user;
+    private String db_password;
+    private Calendar cal = Calendar.getInstance();
+
+    private PreparedStatement ps_getBalance;
+    private PreparedStatement ps_newTransaction;
+
+    public JDBC_DB_CONNECTION(String db_url, String db_user, String db_password) throws SQLException {
+        this.db_url = db_url;
+        this.db_user = db_user;
+        this.db_password = db_password;
+        prepareConnection();
+    }
+
+    @Override
+    public void close() throws Exception {
+        connection.close();
+    }
+
+    public void depositAmount(int amount) throws Exception{
+        if (amount < 0) {
+            try {
+                throw new Exception("Cannot deposit less than 0");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        ps_newTransaction.setInt(1, amount);
+        ps_newTransaction.setDate(2, new java.sql.Date(cal.getTimeInMillis()));
+        ps_newTransaction.executeQuery();
+    }
+
+    public void withDrawAmount(int amount) throws Exception {
+        if (Math.abs(amount) > getBalance()) {
+            try {
+                throw new Exception(String.format("Cannot withdraw more than %d", getBalance()));
+        } catch(Exception e) {
+            e.printStackTrace();
+            }
+        }
+        ps_newTransaction.setInt(1, (Math.abs(amount) * -1));
+        ps_newTransaction.setDate(2, new java.sql.Date(cal.getTimeInMillis()));
+        ps_newTransaction.executeQuery();
+    }
+
+    public int getBalance() throws Exception {
+        try (ResultSet rs = ps_getBalance.executeQuery()) {
+            int sum = -1;
+            while(rs.next()){
+                sum = rs.getInt(1);
+            }
+            return sum;
+        } catch(SQLException e) {
+            throw new Exception(e);
+        }
+    }
+
+    private void prepareConnection() throws SQLException{
+        connection = DriverManager.getConnection(db_url, db_user, db_password);
+        ps_newTransaction = connection.prepareStatement("INSERT INTO bank.transaction " +
+                "(Amount, Date) VALUES (?,?)");
+        ps_getBalance = connection.prepareStatement("SELECT SUM(Amount) FROM bank.transaction");
+    }
+
+}
