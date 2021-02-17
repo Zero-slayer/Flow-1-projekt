e57acf917f326fd690a3741c33b223c77f9926aa
albiek1 <54135401+albiek1@users.noreply.github.com>
2021-02-17 13:22:11 +0100

    added view account list, sorted, and with customer names

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,7 @@
     -Fix add new Customer
     -Add remove customer + account
 */
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static int input = 0;
@@ -19,17 +16,6 @@ public class Main {
         for (Customer t : CustomerList){
             accountList.add(new Account(t));
         }
-        //Account account = new Account(c1);
-        //account.depositAmount(125);
-        //System.out.println(String.format("Ny balance: %d", newBalance));
-
-        //account.depositAmount(325);
-        //System.out.println(String.format("Ny balance: %d", newBalance));
-
-        //account.getBalance();
-        //System.out.println(String.format("Ny balance: %d", newBalance));
-
-        //account.withdrawAmount(50);
         Scanner sc = new Scanner(System.in);
         while(active){
             boolean isCustomer = false;
@@ -43,45 +29,19 @@ public class Main {
             }
             if(customNames.contains(nm)){
                 for (Customer c : CustomerList){
-                    cSel = c;
-                    isCustomer = true;
-                    for (Account a : accountList){
-                        if(a.customer == cSel){
-                            aSel = a;
+                    if(c.getName().equals(nm)){
+                        cSel = c;
+                        isCustomer = true;
+                        for (Account a : accountList){
+                            if(a.customer == cSel){
+                                aSel = a;
+                            }
                         }
                     }
                 }
             }else if(!nm.equals("")){
                 System.out.println(nm + " is not a Customer, returning...");
             }
-            /*for(Customer c : CustomerList){
-                if(){
-                    cSel = c;
-                    isCustomer = true;
-                    for(Account a : accountList){
-                        if(a.customer == cSel){
-                            aSel = a;
-                        }
-                    }
-                }else{
-                    System.out.println(nm + " is not a customer, Returning...");
-                }
-                // ignore spaghetti attempt to add new customer, will fix later
-                /*else if(!nm.equals("")){
-                    System.out.println(nm + " Is not a Customer, do you want to add as new customer?");
-                    String confirm = sc.nextLine();
-                    if(confirm.equals("y")){
-                        System.out.println("Adding new...");
-                        Customer newC = new Customer(nm);
-                        CustomerList.add(newC);
-                        accountList.add(new Account(newC));
-                    }else if(confirm.equals("n")){
-                        System.out.println("Returning...");
-                    }else{
-                        System.out.println("Not a valid input");
-                    }
-                }
-            }*/
             while (isCustomer){
                 System.out.println("Select mode");
                 System.out.println("1: Deposit amount");
@@ -89,8 +49,9 @@ public class Main {
                 System.out.println("3: Show balance");
                 System.out.println("4: Print all transactions");
                 System.out.println("5: Show all customers");
-                System.out.println("6: Change selected Customer");
-                System.out.println("7: Exit");
+                System.out.println("6: Show all accounts");
+                System.out.println("7: Change selected Customer");
+                System.out.println("8: Exit");
                 input = sc.nextInt();
                 assert aSel != null;
                 switch (input){
@@ -126,18 +87,35 @@ public class Main {
                         ArrayList<String> customList = new ArrayList<>();
                         for (Customer customer : CustomerList) {
                             customList.add(customer.getName());
-                            Collections.sort(customList);
                         }
+                        Collections.sort(customList);
                         for (String s : customList){
                             System.out.println(s);
                         }
                         break;
 
                     case (6):
-                        isCustomer = false;
+                        System.out.println("Showing accounts");
+                        HashMap<String, Integer> map = new HashMap<>();
+                        for(Account a : accountList){
+                            map.put(a.getCustomer().getName(), a.getBalance());
+                        }
+                        List<Map.Entry<String, Integer>> list = new LinkedList<>(map.entrySet());
+                        list.sort(Map.Entry.comparingByValue());
+                        HashMap<String, Integer> temp = new LinkedHashMap<>();
+                        for(Map.Entry<String, Integer> aa : list){
+                            temp.put(aa.getKey(), aa.getValue());
+                        }
+                        for(Map.Entry<String, Integer> en : map.entrySet()){
+                            System.out.println(en.getKey() + " : " + en.getValue());
+                        }
                         break;
 
                     case (7):
+                        isCustomer = false;
+                        break;
+
+                    case (8):
                         isCustomer = false;
                         active = false;
                         break;
